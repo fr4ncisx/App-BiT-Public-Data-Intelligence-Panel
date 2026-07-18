002d11e05074b0546b62539db90d673f3e2d97bb
fr4ncisx <fr4ncisx@gmail.com>
2026-07-18 04:11:58 -0300

    fix(backend): improve error handling, logging, and test reliability

--- back/src/main/java/com/appbit/geoanalytics/application/storage/exception/DatasetObjectKeyException.java
+++ back/src/main/java/com/appbit/geoanalytics/application/storage/exception/DatasetObjectKeyException.java
@@ -1,7 +1,8 @@
 package com.appbit.geoanalytics.application.storage.exception;
 
+import com.appbit.geoanalytics.application.exception.ApplicationException;
 import lombok.experimental.StandardException;
 
 @StandardException
-public class DatasetObjectKeyException extends RuntimeException {
+public class DatasetObjectKeyException extends ApplicationException {
 }
